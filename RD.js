ee5270b6a64fe8e66eb425db1e7b13ee4b54a87d
Pyorot <FMasic@hotmail.co.uk>
2017-10-17 23:09:44 +0100

    commit to public remote

--- RD.js
+++ RD.js
@@ -1,3 +1,4 @@
+// Imports Pokemon data
 var data = {};
 data.boss = require('./RDC/Boss.json');
 data.cpm = require('./RDC/CPM.json');
@@ -7,148 +8,153 @@ data.stats = require('./RDC/Stats.json');
 
 module.exports = {
 
-damage:	function (ATK, attacker, move, boss, level) {
-	attacker = data.stats[attacker];
-	move = data.moves[move];
-	defender = data.stats[boss];
-	
-	power = move.Power;
-	STAB = 1; if(move.Type == attacker.Type1 || move.Type == attacker.Type2) {STAB = 1.2}
-	eff = data.eff[move.Type][defender.Type1] * data.eff[move.Type][defender.Type2]
-	base_atk = attacker.Attack;
-	iv_atk = ATK;
-	base_def = defender.Defense;
-	cpm = data.cpm[level];
-	boss_cpm = data.boss[boss].CpM
-	
-	return Math.floor(1 + 0.5*power*STAB*eff*(base_atk+iv_atk)/(base_def+15)*cpm/boss_cpm);
+// Calculates damage dealt given prepared input.
+damage: function (ATK, attacker, move, boss, level) {
+        attacker = data.stats[attacker];
+        move = data.moves[move];
+        defender = data.stats[boss];
+
+        power = move.Power;
+        STAB = 1; if(move.Type == attacker.Type1 || move.Type == attacker.Type2) {STAB = 1.2}
+        eff = data.eff[move.Type][defender.Type1] * data.eff[move.Type][defender.Type2]
+        base_atk = attacker.Attack;
+        iv_atk = ATK;
+        base_def = defender.Defense;
+        cpm = data.cpm[level];
+        boss_cpm = data.boss[boss].CpM
+
+        return Math.floor(1 + 0.5*power*STAB*eff*(base_atk+iv_atk)/(base_def+15)*cpm/boss_cpm);
 },
 
+// Prepares output from prepared inputs.
 breakpoints: function (ATK, attacker, move, boss) {
-	function cl(level) {return module.exports.damage(ATK, attacker, move, boss, level)};
-	dmg_memo = 0;
-	var EOL = require('os').EOL;
-	var DLM = ' | ';
-	output = '__Damage @ level__';
-	for(var i=0; i<79; i++){
-		level = 1+0.5*i;
-		dmg = cl(level);
-		if(dmg > dmg_memo) {
-			if (level >= 10) {output += DLM + dmg + ' @ ' + level};
-			dmg_memo = dmg;
-		}
-	}
-	if (output.length <= 110) {while (output.indexOf(DLM) != -1) {output = output.replace(DLM,EOL)} };
-	if (output == '__Damage @ level__') {output += ' | No breakpoints above level 10 detected.'};
-	return output
+        function damage_closure(level) {return module.exports.damage(ATK, attacker, move, boss, level)};
+        dmg_memo = 0;
+        var EOL = require('os').EOL;    // display delimiter
+        var DLM = ' | ';                                // inline delimiter
+        output = '__Damage @ level__';
+        for(var i=0; i<79; i++){
+                level = 1+0.5*i;
+                dmg = damage_closure(level);
+                if(dmg > dmg_memo) {
+                        if (level >= 10) {output += DLM + dmg + ' @ ' + level};
+                        dmg_memo = dmg;
+                }
+        }
+        if (output.length <= 110) {while (output.indexOf(DLM) != -1) {output = output.replace(DLM,EOL)} };
+        if (output == '__Damage @ level__') {output += ' | No breakpoints above level 10 detected.'};
+        return output
 },
 
-parser: function (input) {
-	function isDigit(n) {
-		return(String(n).charCodeAt(0) >= "0".charCodeAt(0) && n <= "9".charCodeAt(0));
-	}
-	function TitleCase(str)
-	{
-		return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-	}
+// Prepares inputs (returns error string if failed).
+breakpoints_parser: function (input) {
+        function isDigit(n) {
+                return(String(n).charCodeAt(0) >= "0".charCodeAt(0) && n <= "9".charCodeAt(0));
+        }
+        function TitleCase(str)
+        {
+                return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+        }
 
-	input = input.trim();
-	
-	console.log('after command: ' + input)
-	ATK = ''
-	while (input != '' && isDigit(input[0])) {
-		ATK += input[0]; input = input.slice(1)
-	}
-	console.log(ATK)
-	ATK = parseInt(ATK);
-	if (ATK >= 0 && ATK <= 15) {}
-		else {error = "Invalid attack IV; type !breakpoints for help."; console.log(error); return error}
-	input = input.trim();
-	
-	console.log('after ATK IV: ' + input)
-	delimiter = input.indexOf(">");
-	attacker = TitleCase(input.slice(0,delimiter).trim());
-	if (!data.stats[attacker])
-		{error = "Invalid attacker; type !breakpoints for help."; console.log(error); return error}
-	input = input.slice(delimiter+1).trim();
-	
-	console.log('after attacker: ' + input)
-	delimiter = input.indexOf(">");
-	move = TitleCase(input.slice(0,delimiter).trim());
-	if (!data.moves[move])
-		{error = "Invalid move; type !breakpoints for help."; console.log(error); return error}
-	input = input.slice(delimiter+1).trim();
-	
-	console.log('after move: ' + input)
-	boss = TitleCase(input);
-	if (!data.boss[boss])
-		{error = "Invalid raid boss; type !breakpoints for help."; console.log(error); return error}
-	console.log([ATK,attacker,move,boss])
-	return [ATK,attacker,move,boss]
+        input = input.trim();
+
+        console.log('after command: ' + input)
+        ATK = ''
+        while (input != '' && isDigit(input[0])) {
+                ATK += input[0]; input = input.slice(1)
+        }
+        console.log(ATK)
+        ATK = parseInt(ATK);
+        if (ATK >= 0 && ATK <= 15) {}
+                else {error = "Invalid attack IV; type !breakpoints for help."; console.log(error); return error}
+        input = input.trim();
+
+        console.log('after ATK IV: ' + input)
+        delimiter = input.indexOf(">");
+        attacker = TitleCase(input.slice(0,delimiter).trim());
+        if (!data.stats[attacker])
+                {error = "Invalid attacker; type !breakpoints for help."; console.log(error); return error}
+        input = input.slice(delimiter+1).trim();
+
+        console.log('after attacker: ' + input)
+        delimiter = input.indexOf(">");
+        move = TitleCase(input.slice(0,delimiter).trim());
+        if (!data.moves[move])
+                {error = "Invalid move; type !breakpoints for help."; console.log(error); return error}
+        input = input.slice(delimiter+1).trim();
+
+        console.log('after move: ' + input)
+        boss = TitleCase(input);
+        if (!data.boss[boss])
+                {error = "Invalid raid boss; type !breakpoints for help."; console.log(error); return error}
+        console.log([ATK,attacker,move,boss])
+        return [ATK,attacker,move,boss]
 },
 
+// Prepares output from prepared inputs.
 raidiv: function (boss_name, input_cp) {
-	boss = data.stats[boss_name];
-	cpm = data.cpm[20];
-	function cp(atk_iv,def_iv,sta_iv) {
-		atk = boss.Attack + atk_iv;	def = boss.Defense + def_iv; sta = boss.Stamina + sta_iv;
-		return Math.max(10, Math.floor(0.1 * atk * Math.sqrt(def) * Math.sqrt(sta) * cpm * cpm));
-	}
-	output = []
-	for (i = 45; i >= 30; i--){
-		for (a = 15; a >= 10; a--){
-			for (d = 15; d >= 10; d--){
-				for (s = 15; s >= 10; s--){
-					if (a+d+s == i && cp(a,d,s) == input_cp) {
-						IV = a + d + s;
-						result = String(Math.round((IV/45)*100)) + '% (' + String(IV-45) + ') | ' + a + '/' + d + '/' + s
-						output.push(result)
-					}
-				}
-			}
-		}
-	}
-	
-	var EOL = require('os').EOL;
-	if (output.length == 0) {return "What what what what where? How did you get dat number right dere?"};
-	output_text = "__Possible IVs of CP " + input_cp + " " + boss_name + " at level 20__";
-	for (n = 0, len = output.length; n < len; n++) {
-		output_text += EOL + output[n]
-	}
-	return output_text
+        boss = data.stats[boss_name];
+        cpm = data.cpm[20];
+        function cp(atk_iv,def_iv,sta_iv) {
+                atk = boss.Attack + atk_iv;     def = boss.Defense + def_iv; sta = boss.Stamina + sta_iv;
+                return Math.max(10, Math.floor(0.1 * atk * Math.sqrt(def) * Math.sqrt(sta) * cpm * cpm));
+        }
+        output = []
+        for (i = 45; i >= 30; i--){
+                for (a = 15; a >= 10; a--){
+                        for (d = 15; d >= 10; d--){
+                                for (s = 15; s >= 10; s--){             // enforces descending ordering by a+d+s
+                                        if (a+d+s == i && cp(a,d,s) == input_cp) {
+                                                IV = a + d + s;
+                                                result = String(Math.round((IV/45)*100)) + '% (' + String(IV-45) + ') | ' + a + '/' + d + '/' + s
+                                                output.push(result)
+                                        }
+                                }
+                        }
+                }
+        }
+
+        var EOL = require('os').EOL;
+        if (output.length == 0) {return "What what what what where? How did you get dat number right dere?"};
+        output_text = "__Possible IVs of CP " + input_cp + " " + boss_name + " at level 20__";
+        for (n = 0, len = output.length; n < len; n++) {
+                output_text += EOL + output[n]
+        }
+        return output_text
 },
 
+// Prepares inputs (returns error string if failed).
 raidiv_parser: function (input) {
-	function isDigit(n) {
-		return(String(n).charCodeAt(0) >= "0".charCodeAt(0) && n <= "9".charCodeAt(0));
-	}
-	function TitleCase(str)
-	{
-		return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-	}
+        function isDigit(n) {
+                return(String(n).charCodeAt(0) >= "0".charCodeAt(0) && n <= "9".charCodeAt(0));
+        }
+        function TitleCase(str)
+        {
+                return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+        }
 
-	input = input.trim();
-	
-	console.log('after command: ' + input)
-	boss = ''
-	while (input != '' && !isDigit(input[0])) {
-		boss += input[0]; input = input.slice(1)
-	}
-	boss = TitleCase(boss.trim());
-	console.log(boss);
-	if (!data.stats[boss])
-		{error = "Invalid Pokemon / Syntax Error; type !raidiv for help."; console.log(error); return error}
-	
-	console.log('after boss: ' + input)
-	cp = ''
-	while (input != '' && isDigit(input[0])) {
-		cp += input[0]; input = input.slice(1)
-	}
-	console.log(cp)
-	if (cp == '')
-		{error = "Invalid CP; type !raidiv for help."; console.log(error); return error}
-	cp = parseInt(cp);
-	return [boss,cp]
+        input = input.trim();
+
+        console.log('after command: ' + input)
+        boss = ''
+        while (input != '' && !isDigit(input[0])) {
+                boss += input[0]; input = input.slice(1)
+        }
+        boss = TitleCase(boss.trim());
+        console.log(boss);
+        if (!data.stats[boss])
+                {error = "Invalid Pokemon / Syntax Error; type !raidiv for help."; console.log(error); return error}
+
+        console.log('after boss: ' + input)
+        cp = ''
+        while (input != '' && isDigit(input[0])) {
+                cp += input[0]; input = input.slice(1)
+        }
+        console.log(cp)
+        if (cp == '')
+                {error = "Invalid CP; type !raidiv for help."; console.log(error); return error}
+        cp = parseInt(cp);
+        return [boss,cp]
 }
 
 }
--- index.js
+++ index.js
@@ -1,99 +1,185 @@
 const extend = require('util')._extend;
-const Discord = require('discord.js');
-const RD = require('./RD.js');
-const jme = require('./jme.json');
-const key = require('./key.json')[0];
+const Discord = require('discord.js');          // Discord Node.js API
+const https = require('https');                 // HTTP requests
+const RD = require('./RD.js');                  // Parsers and calculators for some functions
+const jme = require('./jme.json');              // JME quote sheet
+const key = require('./key.json').bot;          // Bot token for Discord
+const faqurl = require('./key.json').faq;       // URL of online editable FAQ JSON
 
 const bot = new Discord.Client();
 bot.login(key);
+bot.on('ready', () => {
+        faq = {}; faqupdate()
+        console.log('Bot is ready.');
+        });
 
+// Channel IDs
 pysgeneral =  '293028598024110080'
 pys1 =        '293838131407486980'
+pysstatus =   '298589684492140546'
 pglgeneral =  '260119001018007552'
 pglofftopic = '349689326361247744'
+pglbot =      '341937230337802243'
 pgldev =      '276041020402040833'
 
+// Connection error handling
+bot.on("error", (e) => console.error(e));
+bot.on("disconnect", (e) => console.log(e));
+
+// User message handling
+bot.on('message', (msg) => {
+        // Sends single untagged reply (use msg.reply for single tagged reply)
+        function ureply(reply) {bot.channels.get(msg.channel.id).send(reply).catch(console.error)}
+
+        // Input is !help or ?help
+        if (msg.content == "!help" || msg.content == "?help") {
+                ureply(help);
+        }
+
+        // Input @ bot (JME function)
+    if (msg.isMentioned(bot.user)) {
+                switch(msg.channel.id) {
+                        case pglgeneral: case pglofftopic: case pglbot: case pysgeneral:
+                                post(JME(), msg.channel.id);
+                                break;
+                        case pgldev: case pys1:
+                                msg.reply('Salve magistra.').catch(console.error);
+                                break;
+                }
+    };
+
+        // Input from particular user (meme)
+        if (msg.channel.id == pglgeneral && msg.author.id == "177806056989261824" && msg.content.substring(0,7) == "anyways") {
+                ureply("https://cdn.discordapp.com/attachments/260119001018007552/301774028220399617/image.jpg")
+        };
+
+        // Input starts with !breakpoints
+        if (msg.content == "!breakpoints" || msg.content == "!breakpoints help" ) {
+                ureply(breakpoint_help);
+        } else if (msg.channel.id != pglgeneral && msg.content.slice(0,13) == "!breakpoints ") {
+                ureply(breakpoints(msg.content.slice(13)));
+        };
+
+        // Input starts with !raidiv
+        if (msg.content == "!raidiv" || msg.content == "!raidiv help" ) {
+                ureply(raidiv_help);
+        } else if (msg.channel.id != pglgeneral && msg.content.slice(0,8) == "!raidiv ") {
+                ureply(raidiv(msg.content.slice(8)));
+        };
+
+        // Input is 4-digit number (exposes !raidiv shortcut)
+        if (msg.channel.id != pglgeneral && msg.content.length == 4 && Number.isInteger(cp = parseInt(msg.content))) {
+                output = "";
+                if (cp >= 2184 && cp <= 2275) {output = RD.raidiv("Mewtwo",cp)} else
+                if (cp >= 2011 && cp <= 2097) {output = RD.raidiv("Tyranitar",cp)} else
+                if (msg.createdAt.getMonth() == 8 && cp >= 1847 && cp <= 1930) {output = RD.raidiv("Entei",cp)} else
+                if (msg.createdAt.getMonth() >= 9 && cp >= 1831 && cp <= 1913) {output = RD.raidiv("Raikou",cp)} else
+                if (msg.createdAt.getMonth() >= 9 && cp >= 1538 && cp <= 1613) {output = RD.raidiv("Suicune",cp)};
+                if (!!output) {ureply(output)};
+        };
+
+        // Input starts with ~ (FAQ function)
+        if (msg.content == "~") {
+                faqupdate()
+        } else if (msg.content == "~help") {
+                output = "List of FAQ commands:\n";
+                for (var key in faq) {
+                        if (faq.hasOwnProperty(key)) {output += key + ", "};
+                }
+                ureply(output)
+        } else if (msg.content[0] == "~") {
+                input = msg.content.slice(1)
+                input.trim()
+                if (!!faq[input]) {ureply(faq[input])}
+                else {ureply("Unrecognised command; type ~help for a list of FAQ commands.")}
+        }
+});
+
+// Sand halp
+help =
+          "For personal Pyobot notifications, see <http://pyobot.ga>.\n"
+        + "For a list of FAQ commands, see #welcome-rules on the London Discord.\n"
+        + "For help with RaidBot, type `$help`.\n"
+        + "**!breakpoints** – Calculates attack breakpoints against raid bosses.\n"
+        + "**!raidiv** – Lists the possible IVs of a raid boss encounter given the CP.";
+breakpoint_help =
+          "Syntax: !breakpoints [ATK IV] [Attacker] > [Move] > [Raid Boss]\n"
+        + "E.g. !breakpoints 14 Scizor > Fury Cutter > Celebi\n"
+        + "Breakpoints below level 10 are omitted."
+raidiv_help =
+          "Syntax: !raidiv [Pokemon] [CP]\n"
+        + "E.g. !raidiv Entei 1922\n"
+        + "For active legendaries and Tyranitar, you can type just the number as a shortcut – e.g. `2050`."
+
+// Sends multiple single posts
 function post(listo, channelid) {
     var list = JSON.parse(JSON.stringify(listo));
     if (list.length != 0) {
-        bot.channels.get(channelid).sendMessage(list.shift());
+        bot.channels.get(channelid).send(list.shift()).catch(console.error);
         setTimeout(function(){post(list, channelid)}, 1200);
     }
 }
 
-bot.on('ready', () => {console.log('Bot is ready.');});
-bot.on("error", (e) => console.error(e));
-bot.on("disconnect", (e) => console.log(e));
-
-bot.on('message', (msg) => {
-    if (msg.isMentioned(bot.user)) {
-        if (msg.channel.id == pglgeneral || msg.channel.id == pysgeneral || msg.channel.id == pglofftopic) {
-			post(JME(),msg.channel.id);
-        } else if (msg.channel.id == pgldev || msg.channel.id == pys1) {
-            msg.reply('Word to the rasclarts.');
-        }
-    };
-	
-	if (msg.channel.id == pglgeneral && msg.author.id == "177806056989261824" && msg.content.substring(0,7) == "anyways") {
-		post(["https://cdn.discordapp.com/attachments/260119001018007552/301774028220399617/image.jpg"], msg.channel.id)
-	};
-	
-	if (msg.content == "!help") {
-		post(help(),msg.channel.id);
-	}
-	
-	if (msg.content == "!breakpoints" || msg.content == "!breakpoints help" ) {
-		post(["Syntax: !breakpoints [ATK IV] [Attacker] > [Move] > [Raid Boss]","E.g. !breakpoints 14 Scizor > Fury Cutter > Celebi","Breakpoints below level 10 are omitted"],msg.channel.id);
-	} else if (msg.content.slice(0,13) == "!breakpoints ") {
-		bot.channels.get(msg.channel.id).sendMessage(breakpoints(msg.content.slice(13)));
-	};
-	
-	if (msg.content == "!raidiv" || msg.content == "!raidiv help" ) {
-		post(["Syntax: !raidiv [Pokemon] [CP]","E.g. !raidiv Entei 1922","For active legendaries and Tyranitar, you can type just the number as a shortcut -- e.g. `2050`"],msg.channel.id);
-	} else if (msg.content.slice(0,8) == "!raidiv ") {
-		bot.channels.get(msg.channel.id).sendMessage(raidiv(msg.content.slice(8)));
-	};
-	
-	if (msg.content.length == 4 && Number.isInteger(cp = parseInt(msg.content))) {
-		output = "";
-		if (cp >= 2184 && cp <= 2275) {output = RD.raidiv("Mewtwo",cp)} else
-		if (cp >= 2011 && cp <= 2097) {output = RD.raidiv("Tyranitar",cp)} else
-		if (msg.createdAt.getMonth() == 8 && cp >= 1847 && cp <= 1930) {output = RD.raidiv("Entei",cp)} else
-		if (msg.createdAt.getMonth() >= 9 && cp >= 1831 && cp <= 1913) {output = RD.raidiv("Raikou",cp)} else
-		if (msg.createdAt.getMonth() >= 9 && cp >= 1538 && cp <= 1613) {output = RD.raidiv("Suicune",cp)};
-		if (!!output) {bot.channels.get(msg.channel.id).sendMessage(output)};
-	};
-});
-
+// Exposes JME function
 seed = 0; random_index = 0
 function JME() {
-	if (seed == 11 || seed == 21) {random_index++}
-	while (random_index == seed) {
-		random_index = Math.floor(Math.random()*jme.length);
-		if (random_index == 12 || random_index == 22) {random_index++};
-	}
-	output = jme[random_index];
-	seed = random_index;
-	return output
+        if (seed == 11 || seed == 21) {random_index++}
+        while (random_index == seed) {
+                random_index = Math.floor(Math.random()*jme.length);
+                if (random_index == 12 || random_index == 22) {random_index++};
+        }
+        output = jme[random_index];
+        seed = random_index;
+        return output;
 }
 
-function help() {
-	output = [
-		"For personal Pyobot notifications, see http://pyobot.ga",
-		"> !breakpoints -- Calculates attack breakpoints against raid bosses",
-		"> !raidiv -- Lists the possible IVs of a raid boss encounter given the CP"
-	]
-	return output
-}
-
+// Exposes !breakpoints function
 function breakpoints(input) {
-	parsed = RD.parser(input);
-	if (typeof parsed == 'string') {output = parsed} else {output = RD.breakpoints(...parsed)};
-	return output;
+        parsed = RD.breakpoints_parser(input);
+        if (typeof parsed == 'string') {output = parsed} else {output = RD.breakpoints(...parsed)};
+        return output;
 }
 
+// Exposes !raidiv function
 function raidiv(input) {
-	parsed = RD.raidiv_parser(input);
-	if (typeof parsed == 'string') {output = parsed} else {output = RD.raidiv(...parsed)};
-	return output;
+        parsed = RD.raidiv_parser(input);
+        if (typeof parsed == 'string') {output = parsed} else {output = RD.raidiv(...parsed)};
+        return output;
+}
+
+// Syncs FAQ with JSON on JSONBlob (request maker)
+function faqupdate() {https.get(faqurl, faqcallback)}
+
+// Syncs FAQ with JSON on JSONBlob (response handler)
+function faqcallback(res) {
+        // if the request succeeds, do this:
+        function success(output) {
+                faq = output
+                status_message = "FAQ updated. Commands:\n"
+                for (var key in faq) {
+                        if (faq.hasOwnProperty(key)) {status_message += key + ", "};
+                }
+                bot.channels.get(pysstatus).send(status_message).catch(console.error)
+        }
+
+        // HTTP request boilerplate
+        const {statusCode} = res;
+        let error;
+        if (statusCode !== 200) {
+                error = new Error('Request Failed.\n' + `Status Code: ${statusCode}`);
+                console.error(error.message);
+                res.resume(); // must free memory manually here
+                return;
+        }
+        res.setEncoding('utf8');
+        let rawData = '';
+        res.on('data', (chunk) => {rawData += chunk;});
+        res.on('end', () => {
+                try {
+                        const parsedData = JSON.parse(rawData);
+                        success(parsedData);
+                } catch (e) {
+                        console.error(e.message);
+                }
+        });
 }
